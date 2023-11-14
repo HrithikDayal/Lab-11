78000840fae25f644e16d8509ed2fe6fef01ee0e
HrithikDayal <hrithikdayalsingh@gmail.com>
2023-11-14 21:15:32 +0000

    new python program

---
+++ hthakur11.py
@@ -0,0 +1,12 @@
+#Hrithik Dayal Singh Thakur
+#CSC-321
+#Lab-11
+
+user_input = input("Enter a number: ")
+number = int(user_input)
+
+result = number* number
+
+print(f"The square of {number} is: {result}")
+print("Every dog has its day.")
+
